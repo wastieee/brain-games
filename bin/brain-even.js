9c8bc71d79c28d84d8364f4bd2e9e66a6b1ef9de
wastieee <said.ismailov3421@gmail.com>
2025-05-14 12:02:53 +0300

    five block

---
+++ bin/brain-even.js
@@ -0,0 +1,38 @@
+import readlineSync from 'readline-sync';
+import greetUser from "../src/cli.js";
+
+
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const isEven = (num) => num % 2 === 0;
+
+const runGame = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    const roundsCount = 3;
+
+    for (let i = 0; i < roundsCount; i += 1) {
+        const number = getRandomNumber();
+        console.log(`Question: ${number}`);
+        const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+
+        const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+        if (userAnswer === correctAnswer) {
+            console.log('Correct!');
+        } else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${name}!`);
+};
+
+export default runGame;
+
+runGame();
